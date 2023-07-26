f08d88ba452a6b253bf16ecc0f9d707a4e870c94
Kiran Mandhane <k.mandhane@gmail.com>
2023-07-26 09:28:48 -0600

    add sidebar chat

--- hashlynx/src/App.js
+++ hashlynx/src/App.js
@@ -13,10 +13,29 @@ function App() {
     setSubmitted(true);
   };
 
+useEffect(() => {
+  if(submitted) {
+    console.log(container.current)
+    const widget = new Callbridge.Dashboard(
+    {
+      domain: 'iotum.callbridge.rocks',
+      sso: {
+      token: '7c0d18cbd52f88f89446a9cfe59ee9901f2eaa2e40f7c3474b9ed5b34959d3c9',
+      hostId: '62821',
+    },
+    container: container.current, // Use the container reference
+    },
+    "Team",
+    {layout: "list"}
+    );
+  }
+}, [submitted]);
+
+
 
   if (submitted) {
     return (
-      
+      <div ref={container}></div>
     );
   }
 
