f8042a560c48a4f1a47f443cb636fd59f0fe45a1
Kiran Mandhane <kiran@iotum.com>
2023-07-30 15:20:23 -0400

    added the your-app tab

--- netsapiens/src/App.js
+++ netsapiens/src/App.js
@@ -2,123 +2,100 @@ import * as Callbridge from '@iotum/callbridge-js';
 import React, { useState, useRef, useEffect } from 'react';
 import styles from './submitForm.module.css';
 
+
 function App() {
-  const [token, setToken] = useState('');
-  const [hostId, setHostId] = useState('');
-  const [submitted, setSubmitted] = useState(false);
-  const [activeTab, setActiveTab] = useState('chat');
-  const [showChatWidget, setShowChatWidget] = useState(false);
-  const [showMeetingWidget, setShowMeetingWidget] = useState(false);
+const [token, setToken] = useState('');
+const [hostId, setHostId] = useState('');
+const [submitted, setSubmitted] = useState(false);
+const [yourApp, setYourApp] = useState(true); // New state variable to track the active tab
 
-  // Create a reference for the widget container
-  const container = useRef(null);
-  // Store a reference to the current widget instance
-  const widgetRef = useRef(null);
 
-  const handleTabChange = (tab) => {
-    setActiveTab(tab);
-  };
+// Create a reference for the widget container
+const container = useRef(null);
+// Store a reference to the current widget instance
+const widgetRef = useRef(null);
 
-  const handleTokenChange = (event) => {
-    setToken(event.target.value);
-  };
 
-  const handleHostIdChange = (event) => {
-    setHostId(event.target.value);
-  };
+const handleTokenChange = (event) => {
+  setToken(event.target.value);
+};
 
-  const handleSubmit = () => {
-    setSubmitted(true);
-  };
 
-  const handleChatButtonClick = () => {
-    setShowChatWidget(true);
-    setShowMeetingWidget(false);
-  };
+const handleHostIdChange = (event) => {
+  setHostId(event.target.value);
+};
 
-  const handleMeetingButtonClick = () => {
-    setShowChatWidget(false);
-    setShowMeetingWidget(true);
-  };
 
-  const destroyWidget = () => {
-    if (widgetRef.current) {
-      widgetRef.current.unload();
-      widgetRef.current = null;
+const handleSubmit = () => {
+  setSubmitted(true);
+};
+
+const renderWidget = () => {
+  widgetRef.current = new Callbridge.Dashboard(
+    {
+      domain: 'iotum.callbridge.rocks',
+      sso: {
+        token: token,
+        hostId: hostId,
+    },
+      container: container.current,
     }
-  };
+  );
 
-  const renderWidget = () => {
-    destroyWidget(); // Destroy any existing widget before creating a new one
+    widgetRef.current.toggle(false);
+}
 
-    if (showMeetingWidget) {
-      console.log("meeting")
-      widgetRef.current = new Callbridge.Meeting(
-        {"domain":"iotum.callbridge.rocks",
-        "sso":{
-          "token":token,
-          "hostId":hostId,
-        },
-        "container":container.current
-      }, 
-      undefined, 
-      {"skipJoin":true})
-    };
-
-    if (showChatWidget) {
-      console.log("chat")
-      widgetRef.current = new Callbridge.Dashboard(
-        {
-          domain: 'iotum.callbridge.rocks',
-          sso: {
-            token: token,
-            hostId: hostId,
-          },
-          container: container.current,
-        },
-        'Team',
-        { layout: 'full' }
-      );
-    }
+useEffect(() => {
+  if (submitted) {
+    renderWidget();
   }
+  }, [submitted]);
 
-  useEffect(() => {
-    if (submitted) {
-      renderWidget();
-    }
-    // Cleanup function to remove the widget when the component unmounts
-    return () => {
-      destroyWidget();
-    };
-  }, [submitted, showChatWidget, showMeetingWidget, activeTab]);
+const loadWidget = (service) => {
+  if(service === "None") {
+    widgetRef.current.toggle(false);
+    setYourApp(true);
+  }
+  else {
+    widgetRef.current.load(service, service==="Team" ? "Full" : null);
+    widgetRef.current.toggle(true);  
+    setYourApp(false); 
+  }
+}
+
 
   if (submitted) {
     return (
-    <div className={styles.appContainer}>
-    <div className={styles.verticalTabContainer}>
-      <button onClick={handleChatButtonClick}>Chat</button>
-      <button onClick={handleMeetingButtonClick}>Meeting</button>
-    </div>
-    <div ref={container} className={styles.widgetContainer}></div>
-  </div>
+      <div className={styles.appContainer}>
+        <div className={styles.verticalTabContainer}>
+          <button onClick={() => loadWidget('None')}>Your App</button>
+          <button onClick={() => loadWidget('Team')}>Team Chat</button>
+          <button onClick={() => loadWidget('Drive')}>Drive</button>
+          <button onClick={() => loadWidget('Contacts')}>Contacts</button>
+        </div>
+        {yourApp && <div>Your app goes here</div>}
+
+        <div ref={container} className={styles.widgetContainer}></div>
+      </div>
     );
   }
 
   return (
     <div className="form-wrapper">
-      <form onSubmit={handleSubmit}>
-        <label>
-          SSO Token:
-          <input type="text" value={token} onChange={handleTokenChange} />
-        </label>
-        <br />
-        <label>
-          Host ID:
-          <input type="text" value={hostId} onChange={handleHostIdChange} />
-        </label>
-        <br />
-        <button type="submit">Submit</button>
-      </form>
+    <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: '18px', fontWeight: 'bold'}}>Netsapiens App</div>
+    <form onSubmit={handleSubmit}>
+    <label>
+    SSO Token:
+    <input type="text" value={token} onChange={handleTokenChange} />
+    </label>
+    <br />
+    <label>
+    Host ID:
+    <input type="text" value={hostId} onChange={handleHostIdChange} />
+    </label>
+    <br />
+    <button type="submit">Submit</button>
+    </form>
     </div>
   );
 }
