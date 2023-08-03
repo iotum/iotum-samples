4c95e9798e445e148eae2ba97197aea7325d5a3d
Kiran Mandhane <k.mandhane@gmail.com>
2023-08-02 21:26:01 -0600

    try two widget version

--- netsapiens/src/App.js
+++ netsapiens/src/App.js
@@ -23,30 +23,7 @@
 //   setHostId(event.target.value);
 // };
 
-// // const getUnreadMessages = () => {
-// //   const container = document.createElement('div');
-// //   container.style.display = 'none';
-// //   const invisibleWidget = new Callbridge.Dashboard(
-// //     {
-// //       domain: 'iotum.callbridge.rocks',
-// //       sso: {
-// //         token: token,
-// //         hostId: hostId,
-// //       },
-// //       container: container,
-// //     },
-// //     "Team",
-// //   );
-// //   //this widget listens for the number of unread messages 
-// //   invisibleWidget.on('dashboard.UNREAD_MESSAGES', (data) => {
-// //     const sum = Object.values(data.rooms).reduce((m, n) => m + n, 0);
-// //     setUnreadMessages(sum);
-// //   });
-// // }
-
-
 // const handleSubmit = () => {
-//   // getUnreadMessages();
 //   setSubmitted(true);
 // };
 
@@ -71,6 +48,7 @@
 //     });
 
 //     widgetRef.current.on('dashboard.NAVIGATE', (data) => {
+//       widgetRef.current.toggle(true);
 //       console.log("There was a navigate event");
 //       console.log(data)
 //   });
@@ -135,6 +113,139 @@
 
 // export default App;
 
+//-________________-______________-_______________-____________-____
+
+// import * as Callbridge from '@iotum/callbridge-js';
+// import React, { useState, useRef, useEffect } from 'react';
+// import styles from './submitForm.module.css';
+
+
+// function App() {
+// const [token, setToken] = useState('');
+// const [hostId, setHostId] = useState('');
+// const [submitted, setSubmitted] = useState(false);
+// const [yourApp, setYourApp] = useState(true); 
+// const [unreadMessages, setUnreadMessages] = useState();
+
+// // Create a reference for the widget container
+// const container = useRef(null);
+// // Store a reference to the current widget instance
+// const widgetRef = useRef(null);
+
+// const handleTokenChange = (event) => {
+//   setToken(event.target.value);
+// };
+
+// const handleHostIdChange = (event) => {
+//   setHostId(event.target.value);
+// };
+
+// // const getUnreadMessages = () => {
+// //   const container = document.createElement('div');
+// //   container.style.display = 'none';
+// //   const invisibleWidget = new Callbridge.Dashboard(
+// //     {
+// //       domain: 'iotum.callbridge.rocks',
+// //       sso: {
+// //         token: token,
+// //         hostId: hostId,
+// //       },
+// //       container: container,
+// //     },
+// //     "Team",
+// //   );
+// //   //this widget listens for the number of unread messages 
+// //   invisibleWidget.on('dashboard.UNREAD_MESSAGES', (data) => {
+// //     const sum = Object.values(data.rooms).reduce((m, n) => m + n, 0);
+// //     setUnreadMessages(sum);
+// //   });
+// // }
+
+
+// const handleSubmit = () => {
+//   getUnreadMessages();
+//   setSubmitted(true);
+// };
+
+// const renderWidget = () => {
+//   widgetRef.current = new Callbridge.Dashboard(
+//     {
+//       domain: 'iotum.callbridge.rocks',
+//       sso: {
+//         token: token,
+//         hostId: hostId,
+//     },
+//       container: container.current,
+//     }, 
+//   );
+//     widgetRef.current.toggle(false);
+
+//     widgetRef.current.on('dashboard.NAVIGATE', (data) => {
+//       console.log("There was a navigate event in the main widget");
+//       console.log(data)
+//   });
+// }
+
+// useEffect(() => {
+//   if (submitted) {
+//     renderWidget();
+//   }
+//   }, [submitted]);
+
+// const loadWidget = (service) => {
+//   if(service === "") {
+//     widgetRef.current.toggle(false);
+//     setYourApp(true);
+//   }
+//   else {
+//     widgetRef.current.load(service, service==="Team" ? "Full" : null);
+//     widgetRef.current.toggle(true);  
+//     setYourApp(false); 
+//   }  
+// }
+
+//   if (submitted) {
+//     return (
+//       <div className={styles.appContainer}>
+//         <div className={styles.verticalTabContainer}>
+//           <button onClick={() => loadWidget('')}>Your App</button>
+//           <button onClick={() => loadWidget('Team')} style={{ position: 'relative' }}>
+//           Team Chat
+//             {unreadMessages > 0 && <span className={styles.badge}>{unreadMessages}</span>}
+//           </button>
+//           <button onClick={() => loadWidget('Drive')}>Drive</button>
+//           <button onClick={() => loadWidget('Contacts')}>Contacts</button>
+//         </div>
+//         {yourApp && <div>Your app goes here</div>}
+
+//         <div ref={container} className={styles.widgetContainer}></div>
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <div className="form-wrapper">
+//     <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: '18px', fontWeight: 'bold'}}>Netsapiens App</div>
+//     <form onSubmit={handleSubmit}>
+//     <label>
+//     SSO Token:
+//     <input type="text" value={token} onChange={handleTokenChange} />
+//     </label>
+//     <br />
+//     <label>
+//     Host ID:
+//     <input type="text" value={hostId} onChange={handleHostIdChange} />
+//     </label>
+//     <br />
+//     <button type="submit">Submit</button>
+//     </form>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import * as Callbridge from '@iotum/callbridge-js';
 import React, { useState, useRef, useEffect } from 'react';
 import styles from './submitForm.module.css';
@@ -150,50 +261,27 @@ const [unreadMessages, setUnreadMessages] = useState();
 // Create a reference for the widget container
 const container = useRef(null);
 // Store a reference to the current widget instance
-const widgetRef = useRef(null);
+const chatWidget = useRef(null);
+const widget = useRef(null);
+
 
 const handleTokenChange = (event) => {
   setToken(event.target.value);
 };
 
+
 const handleHostIdChange = (event) => {
   setHostId(event.target.value);
 };
 
-const getUnreadMessages = () => {
-  const container = document.createElement('div');
-  container.style.display = 'none';
-  const invisibleWidget = new Callbridge.Dashboard(
-    {
-      domain: 'iotum.callbridge.rocks',
-      sso: {
-        token: token,
-        hostId: hostId,
-      },
-      container: container,
-    },
-    "Team",
-  );
-  //this widget listens for the number of unread messages 
-  invisibleWidget.on('dashboard.UNREAD_MESSAGES', (data) => {
-    const sum = Object.values(data.rooms).reduce((m, n) => m + n, 0);
-    setUnreadMessages(sum);
-  });
-
-  invisibleWidget.on('dashboard.NAVIGATE', (data) => {
-    console.log("There was a navigate event in the team chat widget");
-    console.log(data)
-});
-}
-
 
 const handleSubmit = () => {
-  getUnreadMessages();
   setSubmitted(true);
 };
 
+//render two widgets 
 const renderWidget = () => {
-  widgetRef.current = new Callbridge.Dashboard(
+  widget.current = new Callbridge.Dashboard(
     {
       domain: 'iotum.callbridge.rocks',
       sso: {
@@ -203,9 +291,23 @@ const renderWidget = () => {
       container: container.current,
     }, 
   );
-    widgetRef.current.toggle(false);
+    widget.current.toggle(false);
 
-    widgetRef.current.on('dashboard.NAVIGATE', (data) => {
+    chatWidget.current = new Callbridge.Dashboard(
+      {
+        domain: 'iotum.callbridge.rocks',
+        sso: {
+          token: token,
+          hostId: hostId,
+      },
+        container: container.current,
+      }, 
+      "Team",
+    );
+      chatWidget.current.toggle(false);
+
+    chatWidget.current.on('dashboard.NAVIGATE', (data) => {
+      chatWidget.current.toggle(true);
       console.log("There was a navigate event in the main widget");
       console.log(data)
   });
@@ -219,15 +321,23 @@ useEffect(() => {
 
 const loadWidget = (service) => {
   if(service === "") {
-    widgetRef.current.toggle(false);
+    widget.current.toggle(false);
+    chatWidget.current.toggle(false);
     setYourApp(true);
   }
-  else {
-    widgetRef.current.load(service, service==="Team" ? "Full" : null);
-    widgetRef.current.toggle(true);  
+  else if (service === "Team") {
+    widget.current.toggle(false);  
+    chatWidget.current.toggle(true);
     setYourApp(false); 
-  }  
+  }  else {
+    widget.current.toggle(true);  
+    chatWidget.current.toggle(false);
+    widget.current.load(service);
+    setYourApp(false); 
+  }
 }
+
+
 
   if (submitted) {
     return (
@@ -248,9 +358,9 @@ const loadWidget = (service) => {
     );
   }
 
+
   return (
     <div className="form-wrapper">
-    <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: '18px', fontWeight: 'bold'}}>Netsapiens App</div>
     <form onSubmit={handleSubmit}>
     <label>
     SSO Token:
@@ -267,5 +377,6 @@ const loadWidget = (service) => {
     </div>
   );
 }
+
 
 export default App;
