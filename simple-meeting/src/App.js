537d11ecf5aa57138c54d53c146aee72a6448346
Kiran Mandhane <kiran@iotum.com>
2023-08-22 22:16:34 -0400

    created start of simple meeting app

---
+++ simple-meeting/src/App.js
@@ -0,0 +1,90 @@
+import * as Callbridge from '@iotum/callbridge-js';
+import React, { useState, useRef, useEffect } from 'react';
+import styles from './submitForm.module.css';
+
+function App() {
+  const [token, setToken] = useState('');
+  const [hostId, setHostId] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+  const [accessCode, setAccessCode] = useState('');
+
+  const container = useRef(null);
+  const widget = useRef(null);
+
+  const handleTokenChange = (event) => {
+    setToken(event.target.value);
+  };
+
+  const handleHostIdChange = (event) => {
+    setHostId(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    setSubmitted(true);
+  };
+
+  const handleAccessCode = (event) => {
+    setAccessCode(event.target.value)
+  }
+
+  const renderWidget = () => {
+    console.log("render meeting widget")
+
+    if (widget.current) {
+      widget.current.unload();
+    }
+    widget.current = new Callbridge.Meeting(
+      {
+        domain: 'iotum.callbridge.rocks',
+        sso: {
+          token: '7c0d18cbd52f88f89446a9cfe59ee9901f2eaa2e40f7c3474b9ed5b34959d3c9',
+          hostId: '62821'
+        },
+        container: container.current,
+      },
+      accessCode, 
+      {
+        skipJoin: true
+      }
+    )
+  }
+
+  if (submitted) {
+    return (
+      <div className={styles.appContainer}>
+        <div className={styles.startButton}>
+          <div className={styles.accessCodeLabel}>Access Code:</div>
+          <input
+            type="text"
+            className={styles.accessCodeInput}
+            value={accessCode}
+            onChange={handleAccessCode}
+          />
+          <button className={styles.startMeetingButton} onClick={() => renderWidget()}>Start Meeting</button>
+        </div>
+        <div ref={container} className={styles.widgetContainer}></div>  
+      </div>
+    );
+  }
+
+  return (
+    <div className="form-wrapper">
+      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: '18px', fontWeight: 'bold'}}>Tabbed Dashboard App</div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          SSO Token:
+          <input type="text" value={token} onChange={handleTokenChange} />
+        </label>
+        <br />
+        <label>
+          Host ID:
+          <input type="text" value={hostId} onChange={handleHostIdChange} />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
